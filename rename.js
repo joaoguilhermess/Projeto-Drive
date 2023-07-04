9b3a30772ae8f20beed451a99efe49d76f4e08b7
Jotagê <71993919+JG-entusiasta@users.noreply.github.com>
2023-07-04 18:06:53 -0300

    Add files via upload

--- rename.js
+++ rename.js
@@ -3,47 +3,14 @@ import Drive from "./drive.js";
 
 class Rename {
 	static async Init() {
+		this.getFilters();
+
 		Drive.getAccounts();
 
 		this.files = 0;
 
-		this.filters = [
-			function(name) {
-				var args = name.split("-");
-
-				if (args[0] == "IMG") {
-					args = args.slice(1);
-
-					args = args.join("_");
-
-					args = args.split(".");
-
-					if (args[0].length == 15) {
-						return args.join(".");
-					}
-				}
-
-				return;
-			},
-			function(name) {
-				var args = name.split("-");
-
-				if (args[0] == "VID") {
-					args = args.slice(1);
-
-					args = args.join("_");
-
-					args = args.split(".");
-
-					if (args[0].length == 15) {
-						return args.join(".");
-					}
-				}
-			}
-		];
-
 		var current = 0;
-		var max = 250;
+		var max = 1;
 
 		for (var a = 0; a < Drive.accounts.length; a++) {
 			await Drive.authDrive(Drive.accounts[a]);
@@ -61,12 +28,12 @@ class Rename {
 						if (name) {
 							if (current > max) {
 								await Drive.renameFile(files[i].id, name);
+								console.log(files[i].name, "=>", name);
 
 								current -= 1;
 							} else {
-								Drive.renameFile(files[i].id, name).then(function(...args) {
-									console.log(...args);
-								}, files[i].name, "=>", name);
+								Drive.renameFile(files[i].id, name);
+								console.log(files[i].name, "=>", name);
 
 								current += 1;
 							}
@@ -75,12 +42,104 @@ class Rename {
 						} else {
 							name = files[i].name;
 
-							console.log(name);
+							try {
+								var args = name.split(".");
+								var args2 = args[0].split("_");
+
+								if (args2[0].length == 8) {
+									if (args2[1].length == 6) {
+										if (["mp4", "jpg", "heic", "jpeg", "png"].includes(args[1])) {
+											continue;
+										}
+									}
+								}
+							} catch {}
+
+							if (k == 0) {
+								console.log(name);
+							}
 						}
 					}
 				}
 			});
 		}
+	}
+
+	static getFilters() {
+		var list = [];
+
+		list.push(function(name) {
+			var args = name.split("-");
+
+			if (args[0] == "IMG") {
+				args = args.slice(1);
+
+				args = args.join("_");
+
+				args = args.split(".");
+
+				if (args[0].length == 15) {
+					return args.join(".");
+				}
+			}
+		});
+
+		list.push(function(name) {
+			var args = name.split("-");
+
+			if (args[0] == "VID") {
+				args = args.slice(1);
+
+				args = args.join("_");
+
+				args = args.split(".");
+
+				if (args[0].length == 15) {
+					return args.join(".");
+				}
+			}
+		});
+
+		list.push(function(name) {
+			var args = name.split(" ");
+
+			if (args[0] == "WhatsApp") {
+				if (args[1] == "Image") {
+					args = args.slice(2);
+
+					args.splice(1, 1);
+
+					args = args.join("_");
+
+					args = args.split(".");
+
+					args[0] += args[1];
+					args[0] += args[2];
+
+					args.splice(1, 2);
+
+					return args.join(".");
+				}
+			}
+		});
+
+		list.push(function(name) {
+			var args = name.split("_");
+
+			if (args[0] == "IMG") {
+				args = args.slice(1);
+
+				if (args[0].length == 8) {
+					var args2 = args[1].split(".");
+
+					if (args2[0].length == 6) {
+						console.log(args.join("_"));
+					}
+				}
+			}
+		});
+
+		this.filters = list;
 	}
 }
 
